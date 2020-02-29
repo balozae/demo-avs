de44133abbb0d58e7b588d3d0bf2d820acfedaa0
balozae <43301216+balozae@users.noreply.github.com>
2020-02-29 18:59:49 +0400

    add Intl polyfill

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from 'redux/store'
-import './index.css'
 import App from './App'
+import './index.css'
+
+/** Intl polyfill (iOS safari) */
+import '@formatjs/intl-pluralrules/polyfill'
+import '@formatjs/intl-pluralrules/dist/locale-data/en'
+import '@formatjs/intl-unified-numberformat/polyfill'
+if (typeof Intl.NumberFormat.__addLocaleData === 'function') {
+  Intl.NumberFormat.__addLocaleData(
+    require('@formatjs/intl-unified-numberformat/dist/locale-data/en.json')
+  )
+}
 
 ReactDOM.render(
   <Provider store={store}>
