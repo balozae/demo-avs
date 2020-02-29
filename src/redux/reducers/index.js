ca48098cc24340cdc145039f34b9f252c980a292
balozae <43301216+balozae@users.noreply.github.com>
2020-02-29 20:46:22 +0400

    remove userReducer (language switcher already removed)

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
-import userReducer from 'redux/reducers/user'
 import ticketReducer from 'redux/reducers/ticket'
 
 const rootReducer = combineReducers({
-  user: userReducer,
   ticket: ticketReducer
 })
 
