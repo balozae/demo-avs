154d883c8054fd7cd7dc45470064b3052054ef65
balozae <43301216+balozae@users.noreply.github.com>
2020-02-29 16:19:14 +0400

    styles: refactor

--- src/components/TicketList/Segment.js
+++ src/components/TicketList/Segment.js
@@ -14,20 +14,21 @@ const Segment = (props) => {
   const stopsCount = stops.length
   const stopsPluralWord = plural(stopsCount, 'пересадка', 'пересадки', 'пересадок')
   const stopsList = stopsCount ? stops.join(', ') : 'прямой'
+  const stopsWord = `${stopsCount === 0 ? 'без' : stopsCount} ${stopsPluralWord}`
 
   return (
-    <div className="ticket__segment">
-      <div className="ticket__segment__meta">
-        <div className="ticket__segment__meta__heading">{origin} &ndash; {destination}</div>
-        <div className="ticket__segment__meta__body">{dateOrigin} &ndash; {dateDestination}</div>
+    <div className="ticket-segment">
+      <div className="ticket-segment__column">
+        <div className="ticket-segment__column__heading">{origin} &ndash; {destination}</div>
+        <div className="ticket-segment__column__body">{dateOrigin} &ndash; {dateDestination}</div>
       </div>
-      <div className="ticket__segment__meta">
-        <div className="ticket__segment__meta__heading">в пути</div>
-        <div className="ticket__segment__meta__body">{formattedDuration.hours}ч {formattedDuration.minutes}м</div>
+      <div className="ticket-segment__column">
+        <div className="ticket-segment__column__heading">в пути</div>
+        <div className="ticket-segment__column__body">{formattedDuration.hours}ч {formattedDuration.minutes}м</div>
       </div>
-      <div className="ticket__segment__meta">
-        <div className="ticket__segment__meta__heading">{stopsCount} {stopsPluralWord}</div>
-        <div className="ticket__segment__meta__body">{stopsList}</div>
+      <div className="ticket-segment__column">
+        <div className="ticket-segment__column__heading">{stopsWord}</div>
+        <div className="ticket-segment__column__body">{stopsList}</div>
       </div>
     </div>
   )
--- src/components/TicketList/Ticket.js
+++ src/components/TicketList/Ticket.js
@@ -7,26 +7,25 @@ const Ticket = (props) => {
   const { price, carrier, segments } = props
 
   return (
-    <div className="ticket__item">
+    <div className="ticket">
       <div className="ticket__heading">
         <div className="ticket__price">
           {formattedNumber(price)} Р
         </div>
-        <div className="ticket__logo">
+        <div className="ticket__carrier">
           <img
             src={`${process.env.REACT_APP_CDN_URL}/110/36/${carrier}.png`}
             srcSet={`${process.env.REACT_APP_CDN_URL}/110/36/${carrier}@2x.png 2x`}
             alt={carrier}
             width="110"
             height="36"
+            className="icon"
           />
         </div>
       </div>
-      <div className="ticket__segments">
-        {segments.map((segment, index) => (
-          <Segment key={index} {...segment} />
-        ))}
-      </div>
+      {segments.map((segment, index) => (
+        <Segment key={index} {...segment} />
+      ))}
     </div>
   )
 }
