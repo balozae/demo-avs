a12ede2ff30b8b1435b37ca099f0cc3517378b37
balozae <43301216+balozae@users.noreply.github.com>
2020-03-01 17:19:16 +0400

    remove redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,14 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
-import logger from 'redux-logger'
 import apiCallMiddleware from 'middlewares/apiCall'
 import rootReducer from 'redux/reducers'
 
-const isDev = process.env.NODE_ENV === 'development'
+// const isDev = process.env.NODE_ENV === 'development'
 
 const middlewares = [
   thunk,
   apiCallMiddleware,
 ]
-
-if (isDev) {
-  middlewares.push(logger)
-}
 
 const store = createStore(
   rootReducer,
