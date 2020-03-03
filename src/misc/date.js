01ff12503c48285453ec82941e276228032163c1
balozae <43301216+balozae@users.noreply.github.com>
2020-03-04 00:19:39 +0400

    bugfix: округление в меньшую сторону

--- src/misc/date.js
+++ src/misc/date.js
@@ -1,7 +1,7 @@
 export const addMinutes = (date, minutes) => new Date(+date + (minutes * 60 * 1000))
 
 export const durationHumanize = (minutes) => {
-  const h = Math.ceil(minutes / 60)
+  const h = Math.floor(minutes / 60)
   const m = minutes % 60
 
   return {
